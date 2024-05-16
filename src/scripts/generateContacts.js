7fbc060a395e81ee15db42e023f47d3a9d331002
wordisstuff <wordisstuff@gmail.com>
2024-05-16 14:25:55 -0500

    3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+for (let i = 0; i < number; i += 1) {
+   createFakeContact();
+}
+    
+};
 
-await generateContacts(5);
+const contacts = await generateContacts(5);
+ 
+await PATH_DB.push(contacts);
+
+// PATH_DB.length === 0?
+//     PATH_DB.push(contacts): PATH_DB.push(...contacts);
